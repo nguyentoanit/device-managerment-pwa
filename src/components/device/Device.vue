010cb6685fe99b06b3c1412f91d025589dc6983d
toan_nv <nguyentoandev@gmail.com>
2018-12-20 11:27:18 +0700

    Add generate QR code library

--- src/components/device/Device.vue
+++ src/components/device/Device.vue
@@ -1,25 +1,29 @@
 <template>
   <div>
-    Device Details
+    QR Code:
+    <qr-code :text="qrContent"></qr-code>
   </div>
 </template>
 
 <script>
+  import VueQRCodeComponent from 'vue-qrcode-component'
 
-export default {
-  name: 'Device',
-  data () {
-    return {
-      var1: ''
+  export default {
+    name: 'Device',
+    data() {
+      return {
+        qrContent: 'Hello QR Code'
+      }
+    },
+    methods: {
+      func1() {
+      }
+    },
+    components: {
+      'qr-code': VueQRCodeComponent
     }
-  },
-  methods: {
-    func1 () {}
-  },
-  components: {}
-}
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
+<style scoped></style>
